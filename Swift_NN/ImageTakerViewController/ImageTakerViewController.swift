b0d3a88af3d3cbbbc26d34cac2dcd0ac2311a28c
Christopher Weaver <crweaver225@yahoo.com>
2019-03-01 11:02:31 -0600

    Initial Commit

---
+++ Swift_NN/ImageTakerViewController/ImageTakerViewController.swift
@@ -0,0 +1,230 @@
+//
+//  ImageTakerViewController.swift
+//  Swift_NN
+//
+//  Created by Christopher Weaver on 1/28/19.
+//  Copyright © 2019 Netronix. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import MetalPerformanceShaders
+import SwiftGifOrigin
+
+class ImageTakerViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    
+    var swiftNN : Swift_Neural_Network!
+    var nnGif : UIImageView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.load_model()
+        self.addPhoto()
+    }
+    
+    func startDisplayingNNGif() {
+        nnGif = UIImageView(frame: CGRect(x: self.view.frame.width * 0.25, y: self.view.frame.height * 0.4, width: self.view.frame.width * 0.5, height: self.view.frame.width * 0.5))
+        nnGif?.image = UIImage.gif(name: "nn")
+        self.view.addSubview(nnGif!)
+    }
+
+    func load_model() {
+        if let path = Bundle.main.path(forResource: "mnist_model2", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as! [String:AnyObject]
+                swiftNN = Swift_Neural_Network()
+                if let type = jsonResult["type"] as? [String] {
+                    swiftNN.layers = swiftNN.convertToLayerTypes(layers: type)
+                }
+                if let activation_functions = jsonResult["activity_functions"] as? [String] {
+                    swiftNN.activation_functions = swiftNN.convertToActivationFunctions(activationFunctions: activation_functions)
+                }
+                if let weights = jsonResult["weights"] as? [Matrix] {
+                    swiftNN.weights = weights
+                }
+                if let filter = jsonResult["filters"] as? [Any] {
+                    var filters : [[[Matrix]]] = []
+                    for filter_data in filter {
+                        if let fd = filter_data as? [[Matrix]] {
+                            filters.append(fd)
+                        } else {
+                            filters.append([[[filter_data as! Array<Double>]]])
+                        }
+                    }
+                    swiftNN.filters = filters
+                }
+                if let stride = jsonResult["stride"] as? [Int] {
+                    swiftNN.stride = stride
+                }
+            } catch {
+            }
+        }
+    }
+    
+    func addPhoto() {
+        AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (granted :Bool) -> Void in
+            if granted == true {
+                DispatchQueue.main.async {
+                    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+                        let imagePicker = UIImagePickerController()
+                        imagePicker.delegate = self
+                        imagePicker.sourceType = .camera
+                        self.present(imagePicker, animated: true, completion: {
+                            self.startDisplayingNNGif()
+                        })
+                    }
+                }
+            } else {
+                let alertView = UIAlertController(title: "Camera could not be accessed", message: "Permission to use this device's camera has been denied. Go to settings to change permissions.", preferredStyle: .alert)
+                alertView.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { action in
+                }))
+                DispatchQueue.main.async {
+                    self.present(alertView, animated: true, completion: nil)
+                }
+            }
+        });
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: {
+        })
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+        if let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
+            let orientationCorrectImage = image.fixOrientation()
+            let invert = orientationCorrectImage.invertedImage()
+            let grayScaleImage = self.convertToGrayScale(image: invert!)
+            let resizedImage = self.resize2(with: grayScaleImage, scaledTo: CGSize(width: 28.0, height: 28.0))
+            let pixelData = pixelValues(fromCGImage: resizedImage.cgImage)
+            var arrayPixelData : [[Double]] = [[]]
+            var arrayIndex : Int = 0
+            for pixel in pixelData.pixelValues! {
+                var modifiedPixel = 0
+                if pixel > 225 {
+                    modifiedPixel = Int(pixel)
+                }
+                let normalizedPixel = (Double(exactly: modifiedPixel) ?? 0.0) / 255.0
+                
+                if arrayPixelData[arrayIndex].count < 28 {
+                    arrayPixelData[arrayIndex].append(normalizedPixel)
+                } else {
+                    arrayPixelData.append([normalizedPixel])
+                    arrayIndex += 1
+                }
+            }
+            picker.dismiss(animated: true, completion: {
+                DispatchQueue.main.async {
+                    if let results = self.swiftNN.executeConvolutionalNeuralNetwork(input: [arrayPixelData]) as? [[Double]] {
+                        print(results)
+                        self.pickNumber(results: results.first!)
+                    }
+                }
+            })
+        }
+    }
+    
+    func pickNumber(results : [Double]) {
+        var index = 0
+        var choosenNumber = 0
+        var currentNumber = 0.0
+        for result in results {
+            if result > currentNumber {
+                choosenNumber = index
+                currentNumber = result
+            }
+            index += 1
+        }
+        let alertView = UIAlertController(title: "Your number is:", message: "\(choosenNumber)", preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title: "OK", style: .cancel, handler: {action in
+            self.addPhoto()
+        }))
+        self.present(alertView, animated: true, completion: nil)
+        self.nnGif?.removeFromSuperview()
+    }
+    
+    func pixelValues(fromCGImage imageRef: CGImage?) -> (pixelValues: [UInt8]?, width: Int, height: Int){
+        var width = 0
+        var height = 0
+        var pixelValues: [UInt8]?
+        if let imageRef = imageRef {
+            width = imageRef.width
+            height = imageRef.height
+            let bitsPerComponent = imageRef.bitsPerComponent
+            let bytesPerRow = 28
+            let totalBytes = height * bytesPerRow
+            
+            let colorSpace = CGColorSpaceCreateDeviceGray()
+            var intensities = [UInt8](repeating: 0, count: totalBytes)
+            
+            let contextRef = CGContext(data: &intensities, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: 0)
+            contextRef?.draw(imageRef, in: CGRect(x: 0.0, y: 0.0, width: CGFloat(width), height: CGFloat(height)))
+            
+            pixelValues = intensities
+        }
+        
+        return (pixelValues, width, height)
+    }
+    
+    func resize2(with image: UIImage, scaledTo newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage ?? UIImage()
+    }
+    
+    func convertToGrayScale(image: UIImage) -> UIImage {
+        let imageRect:CGRect = CGRect(x:0, y:0, width:image.size.width, height: image.size.height)
+        let colorSpace = CGColorSpaceCreateDeviceGray()
+        let width = image.size.width
+        let height = image.size.height
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+        let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+        context?.draw(image.cgImage!, in: imageRect)
+        let imageRef = context!.makeImage()
+        let newImage = UIImage(cgImage: imageRef!)
+        return newImage
+    }
+}
+
+extension UIImage {
+    func fixOrientation() -> UIImage {
+        if self.imageOrientation == UIImage.Orientation.up {
+            return self
+        }
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        if let normalizedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext() {
+            UIGraphicsEndImageContext()
+            return normalizedImage
+        } else {
+            return self
+        }
+    }
+    
+    func invertedImage() -> UIImage? {
+        guard let cgImage = self.cgImage else { return nil }
+        let ciImage = CoreImage.CIImage(cgImage: cgImage)
+        guard let filter = CIFilter(name: "CIColorInvert") else { return nil }
+        filter.setDefaults()
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        let context = CIContext(options: nil)
+        guard let outputImage = filter.outputImage else { return nil }
+        guard let outputImageCopy = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
+        return UIImage(cgImage: outputImageCopy)
+    }
+}
+
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+    return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
+}
+
+
+
---
+++ Swift_NN/Swift_Neural_Network.swift
@@ -0,0 +1,418 @@
+//
+//  Swift_Neural_Network.swift
+//  Swift_NN
+//
+//  Created by Christopher Weaver on 1/30/19.
+//  Copyright © 2019 Netronix. All rights reserved.
+//
+
+import Foundation
+
+enum Layer_Types: String {
+    case Convoluted = "Convoluted"
+    case Recurrent = "Recurrent"
+    case Maxpool = "Maxpool"
+    case Flatten = "Flatten"
+    case Input = "Input"
+    case Fully_Connected = "Fully_Connected"
+}
+
+enum Activation_Function : String {
+    case Sigmoid = "sigmoid"
+    case Relu = "relu"
+    case Leaky_Relu = "leadky relu"
+    case Tanh = "tahn"
+    case SoftMax = "softmax"
+    case Pass = "pass"
+    case Input = "input"
+    case Maxpool = "maxpool"
+    case Flatten = "flatten"
+    
+    func run(input : Matrix) -> Matrix  {
+        switch self {
+        case .Relu:
+            return input
+        case .Sigmoid:
+            return 1.0 / (1.0 + exp(value:-input))
+        case .Pass:
+            return input
+        case .Input:
+            return input
+        case .SoftMax:
+            let result = input[0]
+            let exps = exp(value: result)
+            let sum_of_exps = sum(value: exps)
+            return [exps / sum_of_exps]
+        default:
+            return [[]]
+        }
+    }
+}
+
+func sum(value : [Matrix]) -> Double {
+    var summation : Double = 0.0
+    for dimension in value {
+        summation += sum(value: dimension)
+    }
+    return summation
+}
+
+func return2DMatrixSlice(value: Matrix, startIndexX: Int, startIndexY: Int, slice : Int) -> Matrix {
+    var returnMatrix = Matrix(repeating:  Array(repeating: 0.0, count: slice), count: slice)
+    for second_dimension in startIndexY..<(startIndexY + slice) {
+        for first_diemsion in startIndexX..<(startIndexX + slice) {
+            returnMatrix[second_dimension - startIndexY][first_diemsion - startIndexX] = value[second_dimension][first_diemsion]
+        }
+    }
+    
+    return returnMatrix
+}
+
+func return3DMatrixSlice(value : [Matrix], startIndexX: Int, startIndexY: Int, slice : Int) -> [Matrix] {
+    var returnMatrix = [Matrix](repeating: Array(repeating: Array(repeating: 0.0, count: 3), count: 3), count: value.count)
+    for third_dimension in 0..<(value.count) {
+        for second_dimension in startIndexY..<(startIndexY + slice) {
+            for first_diemsion in startIndexX..<(startIndexX + slice) {
+                returnMatrix[third_dimension][second_dimension - startIndexY][first_diemsion - startIndexX] = value[third_dimension][second_dimension][first_diemsion]
+            }
+        }
+    }
+    
+    return returnMatrix
+}
+
+func sum(value : Matrix) -> Double {
+    var summation : Double = 0.0
+    for row in value {
+        for column in row {
+            summation += column
+        }
+    }
+    return summation
+}
+
+func sum(value : [Double]) -> Double {
+    var summation : Double = 0.0
+    for number in value {
+        summation += number
+    }
+    return summation
+}
+
+func / (lhs: [Double], rhs: Double) -> [Double] {
+    return lhs.map { $0 / rhs }
+}
+
+func / (lhs: Double, rhs: [Double]) -> [Double] {
+    return rhs.map { lhs / $0 }
+}
+
+func / (lhs: Double, rhs: Matrix) -> Matrix {
+    return rhs.map { lhs / $0 }
+}
+
+func + (lhs: Double, rhs: [Double]) -> [Double] {
+    return rhs.map { lhs + $0 }
+}
+
+func + (lhs: Double, rhs: Matrix) -> Matrix {
+    return rhs.map { lhs + $0 }
+}
+
+func exp(value: [Double]) -> [Double] {
+    return value.map { pow(2.718281, $0) }
+}
+
+func exp(value: Matrix) -> Matrix {
+    return value.map { exp(value: $0) }
+}
+
+func max(value : [Double]) -> Double {
+    var maxvalue : Double = 0.0
+    for v in value {
+        if v > maxvalue {
+            maxvalue = v
+        }
+    }
+    return maxvalue
+}
+
+func max(value : Matrix) -> Double {
+    var maxValue : Double = 0.0
+    for row in value {
+        let maxInRow = max(value: row)
+        if maxInRow > maxValue {
+            maxValue = maxInRow
+        }
+    }
+    return maxValue
+}
+
+func - (lhs: Double, rhs: [Double]) -> [Double] {
+    return rhs.map { lhs - $0 }
+}
+
+func - (lhs: Double, rhs: Matrix) -> Matrix {
+    return rhs.map {lhs - $0}
+}
+
+prefix func - (value: [Double]) -> [Double] {
+    return value.map { -$0 }
+}
+
+prefix func - (value: Matrix) -> Matrix {
+    return value.map { -$0 }
+}
+
+func * (lhs : [Double], rhs : [Double]) -> [Double] {
+    var returnArray : [Double] = []
+    for value in 0 ..< lhs.count {
+        returnArray.append(lhs[value] * rhs[value])
+    }
+    return returnArray
+}
+
+typealias Matrix = [[Double]]
+
+class Swift_Neural_Network {
+    
+    var weights : [Matrix] = []
+    var layers : [Layer_Types] = []
+    var stride : [Int] = []
+    var filters : [[[Matrix]]] = []
+    var hidden_layer_results : [[Double]] = []
+    var activation_functions : [Activation_Function] = []
+
+    func convertToActivationFunctions(activationFunctions : [String]) -> [Activation_Function] {
+        var returnArray : [Activation_Function] = []
+        for activationFunction in activationFunctions {
+            switch activationFunction {
+            case "sigmoid":
+                returnArray.append(Activation_Function.Sigmoid)
+            case "relu":
+                returnArray.append(Activation_Function.Relu)
+            case "leaky relu":
+                returnArray.append(Activation_Function.Leaky_Relu)
+            case "tahn":
+                returnArray.append(Activation_Function.Tanh)
+            case "softmax":
+                returnArray.append(Activation_Function.SoftMax)
+            case "pass":
+                returnArray.append(Activation_Function.Pass)
+            case "input":
+                returnArray.append(Activation_Function.Input)
+            case "maxpool":
+                returnArray.append(Activation_Function.Maxpool)
+            case "flatten":
+                returnArray.append(Activation_Function.Flatten)
+            default:
+                print("failed to find activiation function type")
+            }
+        }
+        return returnArray
+    }
+
+    func convertToLayerTypes(layers : [String]) -> [Layer_Types] {
+        var returnArray : [Layer_Types] = []
+        for layer in layers {
+            switch layer {
+            case "Fully_Connected":
+                returnArray.append(Layer_Types.Fully_Connected)
+            case "Input":
+                returnArray.append(Layer_Types.Input)
+            case "Flatten":
+                returnArray.append(Layer_Types.Flatten)
+            case "Maxpool":
+                returnArray.append(Layer_Types.Maxpool)
+            case "Convoluted":
+                returnArray.append(Layer_Types.Convoluted)
+            case "Recurrent":
+                returnArray.append(Layer_Types.Recurrent)
+            default:
+                print("oh well")
+            }
+        }
+        return returnArray
+    }
+    
+    func multiply( _ matrixA:[Matrix], _ matrixB:[Matrix]) -> [Matrix] {
+        var results : [Matrix] = [Matrix](repeating: Array(repeating: Array(repeating: 0.0, count: matrixA[0].count), count: matrixA[0].count), count: matrixA.count)
+        for tensor_row in 0 ..< results.count {
+            results[tensor_row] = multiplyNotDot(matrixA[tensor_row], matrixB[tensor_row])
+        }
+        return results
+    }
+    
+    func multiplyNotDot( _ matrixA:[[Double]], _ matrixB:[[Double]]) -> Matrix {
+        var result:[[Double]] = [[Double]]( repeating: [Double]( repeating: 0, count: matrixB[0].count ), count: matrixA.count )
+        for result_row in 0 ..< result.count {
+            for result_column in 0 ..< result[0].count {
+                result[result_row][result_column] = matrixA[result_row][result_column] * matrixB[result_row][result_column]
+            }
+        }
+        return result
+    }
+    
+    func multiply( _ matrixA:[[Double]], _ matrixB:[[Double]]) -> [[Double]] {
+        if matrixA[0].count != matrixB.count {
+            print( "Illegal matrix dimensions! \(matrixA[0].count) * \(matrixB[0].count)" )
+            return [[]]
+        }
+        var result:[[Double]] = [[Double]]( repeating: [Double]( repeating: 0, count: matrixB[0].count ), count: matrixA.count )
+        for result_row in 0 ..< result.count {
+            for result_column in 0 ..< result[0].count {
+                var transposedColumn : [Double] = []
+                for matBColumn in matrixB {
+                    transposedColumn.append(matBColumn[result_column])
+                }
+                result[result_row][result_column] = sum(value: matrixA[result_row] * transposedColumn)
+            }
+        }
+        
+        return result
+    }
+    
+    func prettyPrintMatrix( _ matrix:[[Double]] ) {
+        for array in matrix {
+            print( array )
+        }
+    }
+    
+    func executeConvolutionalNeuralNetwork(input : [Matrix]) -> Any {
+        var current_results : Any!
+        var layerIndex : Int = 0
+        var filterIndex : Int = 0
+        for layer in self.layers {
+            switch layer {
+            case .Convoluted:
+                current_results = convolution(input_data: current_results as! [Matrix], filters: self.filters[filterIndex], stride: self.stride[filterIndex], activation_fuction: self.activation_functions[layerIndex])
+                filterIndex += 1
+                print("Convolutional")
+            case .Maxpool:
+                current_results = maxpool(input_data: current_results as! [Matrix], window_size: 2, stride: self.stride[filterIndex])
+                filterIndex += 1
+                print("Maxpool")
+            case .Flatten:
+                current_results = flatten(input_data: current_results as! [Matrix])
+                filterIndex += 1
+                print("Flatten")
+            case .Input:
+                print("Input")
+                current_results = input
+                layerIndex -= 1
+            case .Fully_Connected:
+                if (current_results as? Matrix) != nil {
+                } else {
+                    current_results = [current_results]
+                }
+                current_results = forwardPassDenseLayer(input: current_results as! Matrix, activation_function: self.activation_functions[layerIndex], weights: self.weights[layerIndex - filterIndex])
+                print("Dense")
+            case .Recurrent:
+                print("Recurrent")
+            }
+            layerIndex += 1
+        }
+        return current_results
+    }
+ 
+    func executeNueralNetwork(input : Matrix) {
+        var current_results : Matrix = [[]]
+        var layerIndex : Int = 0
+        for layer in self.layers {
+            switch layer {
+            case .Convoluted:
+                print("Convolutional")
+            case .Maxpool:
+                print("Maxpool")
+            case .Flatten:
+                print("Flatten")
+            case .Input:
+                print("Input")
+                current_results = input
+                layerIndex -= 1
+            case .Fully_Connected:
+                current_results = forwardPassDenseLayer(input: current_results, activation_function: self.activation_functions[layerIndex], weights: self.weights[layerIndex])
+                print("Dense")
+            case .Recurrent:
+                print("Recurrent")
+            }
+            layerIndex += 1
+        }
+        print(current_results)
+    }
+    
+    func forwardPassDenseLayer(input : Matrix, activation_function : Activation_Function, weights : Matrix) -> Matrix {
+        let dot_results = multiply(input, weights)
+        let hidden_layer_results = activation_function.run(input: dot_results)
+        return hidden_layer_results
+    }
+
+    func convolution(input_data : [Matrix], filters : [[Matrix]], stride : Int, activation_fuction : Activation_Function) -> [Matrix] {
+        let filter_height = filters[0][0].count
+        let image_height = input_data[0].count
+        let outputWidthAndHeight = ((image_height - filter_height) / stride) + 1
+        var output = [Matrix](repeating: Array(repeating: Array(repeating: 0.0, count: outputWidthAndHeight), count: outputWidthAndHeight), count: filters.count)
+        var filter_count = 0
+        for current_filter in filters {
+            var current_y = 0
+            var out_y = 0
+            while current_y + filter_height <= image_height {
+                var current_x = 0
+                var out_x = 0
+                while current_x + filter_height <= image_height {
+                    let s = return3DMatrixSlice(value: input_data, startIndexX: current_x, startIndexY: current_y, slice: filter_height)
+                    let r = multiply(current_filter, s)
+                    let rr = sum(value: r)
+                    output[filter_count][out_y][out_x] = max(0.0, rr)
+                    current_x += stride
+                    out_x += 1
+                }
+                current_y += stride
+                out_y += 1
+            }
+            filter_count += 1
+        }
+        return output
+    }
+    
+    func maxpool(input_data : [Matrix], window_size : Int, stride : Int) -> [Matrix] {
+        let number_of_channels = input_data.count
+        let input_height = input_data[0].count
+        let height = ((input_height - window_size) / stride) + 1
+        
+        var downsampled = [Matrix](repeating: Array(repeating: Array(repeating: 0.0, count: height), count: height), count: number_of_channels)
+        var channel_count = 0
+        for _ in input_data {
+            var current_y = 0
+            var out_y = 0
+            while current_y + window_size <= input_height {
+                var current_x = 0
+                var out_x = 0
+                while current_x + window_size <= input_height {
+                    let input_slice = return2DMatrixSlice(value: input_data[channel_count], startIndexX: current_x, startIndexY: current_y, slice: window_size)
+                    downsampled[channel_count][out_y][out_x] = max(value: input_slice)
+                    current_x += stride
+                    out_x += 1
+                }
+                current_y += stride
+                out_y += 1
+            }
+            channel_count += 1
+        }
+        return downsampled
+    }
+    
+    func flatten(input_data : [Matrix]) -> [Double] {
+        var returnArray : [Double] = []
+        for channel in input_data {
+            for row in channel {
+                for column in row {
+                    returnArray.append(column)
+                }
+            }
+        }
+        return returnArray
+    }
+}
+
+
